2db8d0562319dcd6f1b1ddc4478b8554c51d479a
swarnjit <swarnjitchahal@gmail.com>
2020-03-28 19:47:31 -0400

    Updates

---
+++ precache-manifest.e04ed1289d36af1f5655153141a4e071.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "955ba665ec754ce215149e444fb2b7e1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "805b78b2f9dee66e020c",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6a0a5bd8c8489d47df8c",
+    "url": "/portfolio/static/js/2.7383083c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.7383083c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "805b78b2f9dee66e020c",
+    "url": "/portfolio/static/js/main.5f92203c.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "7871c3731f8c45ced8c544337f4c3f81",
+    "url": "/portfolio/static/media/me.7871c373.png"
+  }
+]);

0925a4f147d939839175f433f3d13c78010152c0
swarnjit <swarnjitchahal@gmail.com>
2020-03-28 19:39:57 -0400

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,115 @@
+import React, { useState } from "react";
+import RightMenu from "@material-ui/core/Drawer";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  ListItem,
+  ListItemIcon,
+  Avatar,
+  List,
+  Divider,
+  Typography,
+  Box,
+  ListItemText
+} from "@material-ui/core";
+import {
+  ArrowBack,
+  AssignmentInd,
+  Home,
+  Apps,
+  ContactMail
+} from "@material-ui/icons";
+import { makeStyles } from "@material-ui/styles";
+import avatar from "../images/me.png";
+
+const useStyles = makeStyles(theme => ({
+  menuContainer: {
+    width: 250,
+    background: "#511",
+    height: "100%"
+  },
+  avatar: {
+    display: "block",
+    margin: "0.5rem auto",
+    width: 130,
+    height: 130
+  },
+  listItem: {
+    color: "lightblue"
+  }
+}));
+const menuItems = [
+  {
+    listIcon: <Home />,
+    listText: "Home"
+  },
+  {
+    listIcon: <AssignmentInd />,
+    listText: "Resume"
+  },
+  {
+    listIcon: <Apps />,
+    listText: "Portfolio"
+  },
+  {
+    listIcon: <ContactMail />,
+    listText: "Contacts"
+  }
+];
+
+const sideList = slider => {};
+const Navbar = () => {
+  const [state, setState] = useState({
+    right: false
+  });
+
+  const toggleSlider = (slider, open) => () => {
+    setState({ ...state, [slider]: open });
+  };
+  const classes = useStyles();
+
+  const sideList = slider => (
+    <Box className={classes.menuContainer} component="div">
+      <Avatar className={classes.avatar} src={avatar} alt="Swarnjit Chahal" />
+      <Divider />
+      <List>
+        {menuItems.map((IsItem, key) => (
+          <ListItem button key={key}>
+            <ListItemIcon className={classes.listItem}>
+              {IsItem.listIcon}
+            </ListItemIcon>
+            <ListItemText
+              className={classes.listItem}
+              primary={IsItem.listText}
+            />
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+  return (
+    <>
+      <Box component="nav">
+        <AppBar position="static" style={{ background: "#222" }}>
+          <Toolbar>
+            <IconButton onClick={toggleSlider("right", true)}>
+              <ArrowBack style={{ color: "orange" }} />
+            </IconButton>
+            <Typography variant="h5" style={{ color: "lightblue" }}>
+              Swarnjit's Portfolio
+            </Typography>
+            <RightMenu
+              open={state.right}
+              onClose={toggleSlider("right", false)}
+            >
+              {sideList("right")}
+            </RightMenu>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </>
+  );
+};
+
+export default Navbar;

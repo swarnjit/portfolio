4b6598705ab068b880cc59a9ef0d2406af710973
swarnjit <swarnjitchahal@gmail.com>
2020-03-28 22:20:13 -0400

    added header and background image

---
+++ src/components/Header.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Typography, Avatar, Grid, Box } from "@material-ui/core";
+import avatar from "../images/me.png";
+import Typed from "react-typed";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles(theme => ({
+  avatar: {
+    width: 120,
+    height: 120,
+    margin: 8
+  },
+  title: {
+    color: "orange"
+  },
+  subtitle: {
+    color: "lightblue",
+    marginBottom: "3rem"
+  },
+  typedOutput: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: "100vw",
+    textAlign: "center",
+    zIndex: 1
+  }
+}));
+
+const Header = () => {
+  const classes = useStyles();
+  return (
+    <Box className={classes.typedOutput}>
+      <Grid container justify="center">
+        <Avatar className={classes.avatar} src={avatar} alt="Swarnjit Chahal" />
+      </Grid>
+      <Typography className={classes.title} variant="h4">
+        <Typed strings={["Swarnjit Chahal"]} typeSpeed={40} />
+      </Typography>
+      <Typography className={classes.subtitle} variant="h4">
+        <Typed
+          strings={["Java Developer", "React Developer"]}
+          typeSpeed={40}
+          backSpeed={50}
+          loop
+        />
+      </Typography>
+    </Box>
+  );
+};
+
+export default Header;
--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./Navbar";
+import Header from "./Header";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <Header />
     </div>
   );
 };
